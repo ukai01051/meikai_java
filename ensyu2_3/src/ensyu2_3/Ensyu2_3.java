bb4c174386fac42eb1b95f0cc6c259a5b7b3f412
ukai01051 <a@abc.com>
2023-04-04 17:01:18 +0900

    演習2-3

---
+++ ensyu2_3/src/ensyu2_3/Ensyu2_3.java
@@ -0,0 +1,15 @@
+package ensyu2_3;
+import java.util.Scanner;
+
+public class Ensyu2_3 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		Scanner scan = new Scanner(System.in);
+		System.out.print("整数値:");
+		int a = scan.nextInt();
+		
+		System.out.println(a +"と入力しましたね。");
+	}
+
+}
