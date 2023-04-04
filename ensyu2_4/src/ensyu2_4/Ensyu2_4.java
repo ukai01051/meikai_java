a6e30a17889ca33443b8a3f3b5634d6bbef248fe
ukai01051 <a@abc.com>
2023-04-04 17:04:36 +0900

    演習2-4

---
+++ ensyu2_4/src/ensyu2_4/Ensyu2_4.java
@@ -0,0 +1,18 @@
+package ensyu2_4;
+
+import java.util.Scanner;
+
+public class Ensyu2_4 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		Scanner scan = new Scanner(System.in);
+		System.out.print("整数値:");
+		int a = scan.nextInt();
+		
+		System.out.println("10加えた値は"+(a+10) +"です。");
+		System.out.println("10減じた値は"+(a-10) +"です。");
+		
+	}
+
+}
