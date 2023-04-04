c7290db4f49f00790127b060fb6b20ba16a170ff
ukai01051 <a@abc.com>
2023-04-04 16:58:09 +0900

    演習2-2

---
+++ ensyu2_2/src/ensyu2_2/Ensyu2_2.java
@@ -0,0 +1,23 @@
+package ensyu2_2;
+
+public class Ensyu2_2 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int x;
+		int y;
+		int z;
+		
+		x=63;
+		y=18;
+		z=73;
+		
+		System.out.println("xの値は"+x+"です。");
+		System.out.println("yの値は"+y+"です。");
+		System.out.println("zの値は"+z+"です。");
+		System.out.println("合計は"+(x+y+z)+"です。");
+		System.out.println("平均は"+(x+y+z)/3+"です。");
+
+	}
+
+}
