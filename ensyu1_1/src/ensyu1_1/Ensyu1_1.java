aea775c15e85c91d05d5ae49fd3c55f6c7806eac
ukai01051 <a@abc.com>
2023-04-04 17:49:52 +0900

    演習1-1

--- ensyu1_1/src/ensyu1_1/Ensyu1_1.java
+++ ensyu1_1/src/ensyu1_1/Ensyu1_1.java
@@ -1,10 +1,11 @@
+//パッケージ宣言
 package ensyu1_1;
 
+//mainクラス
 public class Ensyu1_1 {
-
+	//mainメソッド
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
+		//HelloWorldを表示
 		System.out.println("Hello World");
 	}
-
 }
