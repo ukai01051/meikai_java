31fa9e0ebe1e6fc14b6bacbfb1399b2292103970
ukai01051 <a@abc.com>
2023-04-05 16:32:20 +0900

    演習2-7

---
+++ ensyu2_7/src/ensyu2_7/Ensyu2_7.java
@@ -0,0 +1,38 @@
+//ensyu2_7のパッケージ宣言
+package ensyu2_7;
+//乱数生成のためのインポート文
+import java.util.Random;
+/*
+ * クラス名:Ensyu2_7
+ * 概要:3つの乱数を生成して表示
+ * 作成者:ukai
+ * 作成日:2023/04/05
+ */
+public class Ensyu2_7 {
+	/*
+	 * 関数名:main
+	 * 概要:3つの乱数を生成して表示
+	 * 引数:なし
+	 * 戻り値:なし
+	 * 作成者:ukai
+	 * 作成日:2023/04/05
+	 */
+	public static void main(String[] args) {
+		//Randomクラスの引数なしのインスタンスを作成
+		Random randomGeneration = new Random();
+		//1～9の乱数を生成変数に代入
+		int firstRandom=randomGeneration.nextInt(9)+1;
+		//-1～-9の乱数を生成変数に代入
+		int secondRandom=randomGeneration.nextInt(9)-9;
+		//10～99の乱数を生成して変数に代入
+		int thirdRandom=randomGeneration.nextInt(89)+10;
+		//1～9の乱数を表示
+		System.out.println(firstRandom);
+		//-1～-9の乱数を表示
+		System.out.println(secondRandom);
+		//10～99の乱数を表示
+		System.out.println(thirdRandom);
+				
+	}
+
+}
