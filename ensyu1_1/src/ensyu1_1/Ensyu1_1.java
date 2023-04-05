75356f8d6dd04fb8c64357034c40c26e55b1bee2
ukai01051 <a@abc.com>
2023-04-05 09:39:09 +0900

    コメントを追加しました。

--- ensyu1_1/src/ensyu1_1/Ensyu1_1.java
+++ ensyu1_1/src/ensyu1_1/Ensyu1_1.java
@@ -1,16 +1,21 @@
-/*
- *クラス名:Ensyu1_1
- *概要：HelloWorldを表示するプログラム
- *作成者：鵜飼咲葵
- *作成日；2023/04/04
- */
 
 //パッケージ宣言
 package ensyu1_1;
-
-//mainクラス
+/*
+ *クラス名:Ensyu1_1
+ *概要:HelloWorldを表示する
+ *作成者:ukai
+ *作成日:2023/04/04
+ */
 public class Ensyu1_1 {
-	//mainメソッド
+	/*
+	 * 関数名:main
+	 * 概要:Helloworldを表示する
+	 * 引数;なし
+	 * 戻り値:なし
+	 * 作成者:ukai
+	 * 作成日:2023/04/04
+	 */
 	public static void main(String[] args) {
 		//HelloWorldを表示
 		System.out.println("Hello World");
