ca354912fa5f6ab90d9b364d64158b408764f9ca
ukai01051 <a@abc.com>
2023-04-05 15:21:41 +0900

    演習2-1

--- ensyu1_2/src/ensyu1_2/Ensyu1_2.java
+++ ensyu1_2/src/ensyu1_2/Ensyu1_2.java
@@ -1,10 +1,25 @@
+//ensyu1_2パッケージ宣言
 package ensyu1_2;
 
+/*
+ *クラス名:Ensyu1_2
+ *概要:名前を一文字ごとに改行して表示する
+ *作成者:ukai
+ *作成日:2023/04/05
+ */
 public class Ensyu1_2 {
-
+	/*
+	 * 関数名:main
+	 * 概要:名前を一文字ごとに改行して表示する
+	 * 引数:なし
+	 * 戻り値:なし
+	 * 作成者:uakai
+	 * 作成日:2023/04/05
+	 */
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
+		//一文字ごとに\nで改行して表示
 		System.out.println("鵜\n飼\n咲\n葵");
+
 	}
 
 }
