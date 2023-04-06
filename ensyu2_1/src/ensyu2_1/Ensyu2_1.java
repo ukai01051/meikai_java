0c1119ed7f853a34c92e29a058e238a168a8a584
ukai01051 <a@abc.com>
2023-04-06 18:09:04 +0900

    演習2-1　修正しました。

--- ensyu2_1/src/ensyu2_1/Ensyu2_1.java
+++ ensyu2_1/src/ensyu2_1/Ensyu2_1.java
@@ -16,11 +16,16 @@ public class Ensyu2_1 {
 	 * 作成日:2023/04/05
 	 */
 	public static void main(String[] args) {
-		//1つめの変数を宣言し少数部を持つ実数値を代入
-		double firstNumber=63.3;
-		//2つめの変数を宣言し少数部を持つ実数値を代入
-		double secondNumber=18.8;
+		//1つめの変数を宣言
+		int firstNumber;
+		//2つめの変数を宣言
+		int secondNumber;
 		
+		//1つ目の変数に少数部を持つ実数値を代入
+		firstNumber=18.8;
+		//1つ目の変数に少数部を持つ実数値を代入
+		secondNumber=63.3;
+				
 		//1つめの変数の値を表示
 		System.out.println("firstNumberの値は"+firstNumber+"です。");
 		//2つめの変数の値を表示
