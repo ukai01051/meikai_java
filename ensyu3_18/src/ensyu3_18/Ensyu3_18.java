b2eca34a4e7b3f7af2d1338a1851c106f03eb7b2
ukai01051 <a@abc.com>
2023-04-10 18:06:23 +0900

    演習3-18 修正しました。

--- ensyu3_18/src/ensyu3_18/Ensyu3_18.java
+++ ensyu3_18/src/ensyu3_18/Ensyu3_18.java
@@ -18,36 +18,36 @@ public class Ensyu3_18 {
 	 * 作成日:2023/04/06
 	 */
 	public static void main(String[] args) {
-	//Scannerクラスのインスタンスを作成
-	//引数:標準入力System.in
-	Scanner scanInt= new Scanner(System.in);
-	//月の入力を促す
-	System.out.println("月を入力してください。");
-	//インスタンスから値を受け取る
-	int monthValue = scanInt.nextInt();
+		//Scannerクラスのインスタンスを作成
+		//引数:標準入力System.in
+		Scanner scanInt= new Scanner(System.in);
+		//月の入力を促す
+		System.out.print("月を入力してください。:");
+		//インスタンスから値を受け取る
+		int monthValue = scanInt.nextInt();
 	
-	//入力された値によって場合分け
-	switch(monthValue) {
-	//3,4,5の場合を表示
-	case 3,4,5 :System.out.println("季節は春です。");
-	//プログラム終了
-	break;
-	//6,7,8の場合夏を表示
-	case 6,7,8 :System.out.println("季節は夏です。");
-	//プログラム終了
-	break;
-	//9,10,11の場合秋を表示
-	case 9,10,11 :System.out.println("季節は秋です。");
-	//プログラム終了
-	break;
-	//12,1,2の場合冬を表示
-	case 12,1,2 :System.out.println("季節は冬です。");
-	//プログラム終了
-	break;
-	//不正な値入力
-	default : System.out.println("不正な値が入力されました。");
-	//プログラム終了
-	break;
+		//入力された値によって場合分け
+		switch(monthValue) {
+		//3,4,5の場合を表示
+		case 3,4,5 :System.out.println("季節は春です。");
+		//プログラム終了
+		break;
+		//6,7,8の場合夏を表示
+		case 6,7,8 :System.out.println("季節は夏です。");
+		//プログラム終了
+		break;
+		//9,10,11の場合秋を表示
+		case 9,10,11 :System.out.println("季節は秋です。");
+		//プログラム終了
+		break;
+		//12,1,2の場合冬を表示
+		case 12,1,2 :System.out.println("季節は冬です。");
+		//プログラム終了
+		break;
+		//不正な値入力
+		default : System.out.println("不正な値が入力されました。");
+		//プログラム終了
+		break;
 		}
 
 	}
