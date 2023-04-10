ab22d8b9eb0399f3796f8c5e8ab8dd602ec1ca41
ukai01051 <a@abc.com>
2023-04-10 17:17:08 +0900

    演習3-14 修正しました。

--- ensyu3_14/src/ensyu3_14/Ensyu3_14.java
+++ ensyu3_14/src/ensyu3_14/Ensyu3_14.java
@@ -3,15 +3,15 @@ package ensyu3_14;
 //標準出力のためのインポート文
 import java.util.Scanner;
 /*
- * クラス名:Ensyu_
- * 概要:入力した2つの整数値を昇順にソートし等しい場合にはメッセージを表示する
+ * クラス名:Ensyu3_14
+ * 概要:入力した2つの整数値を表示し等しい場合にはメッセージを表示する
  * 作成者:ukai
  * 作成日:2023/04/06
  */
 public class Ensyu3_14 {
 	/*
 	 * 関数名:main
-	 * 概要:入力した2つの整数値を昇順にソートし等しい場合にはメッセージを表示する
+	 * 概要:入力した2つの整数値を表示し等しい場合にはメッセージを表示する
 	 * 引数:なし
 	 * 戻り値:なし
 	 * 作成者:ukai
@@ -29,21 +29,10 @@ public class Ensyu3_14 {
 		System.out.print("変数b:");
 		//インスタンスから入力された値を受け取る
 		int secondNumber=scannerInt.nextInt();
-		//aに入力した数値がbに入力した数値より大きい場合
-		if(firstNumber>secondNumber) {
-			//ソート用の変数を宣言してaに入力した変数を移動
-			int sortValue=firstNumber;
-			//bに入力した変数をfirstNumberに代入
-			firstNumber=secondNumber;
-			//aとbに入力した値を交換しソート完了
-			secondNumber=sortValue;
-		//2つの整数が違う場合
-		}if(firstNumber!=secondNumber) 
-			//ソートする趣旨を表示
-			System.out.println("a≦bとなるようにソートしました。");
-		//昇順に表示
+			
+		//変数aを表示
 		System.out.println("変数aは"+firstNumber+"です。");
-		//昇順に表示j
+		//変数bを表示j
 		System.out.println("変数bは"+secondNumber+"です。");
 		//2つの整数が等しい場合
 		if(firstNumber==secondNumber)
