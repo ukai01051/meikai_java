ab5db353f4c2463de24e0dbb1585fbc1294cefdd
ukai01051 <a@abc.com>
2023-04-11 16:48:01 +0900

    演習4-10 修正しました。

--- ensyu4_10/src/ensyu4_10/Ensyu4_10.java
+++ ensyu4_10/src/ensyu4_10/Ensyu4_10.java
@@ -9,7 +9,7 @@ import java.util.Scanner;
  * 作成者:ukai
  * 作成日:2023/04/07
  */
-public class Ensyu {
+public class Ensyu4_10 {
 	/*
 	 * 関数名:main
 	 * 概要:入力された値の数だけ＊を表示
@@ -23,17 +23,29 @@ public class Ensyu {
 		//Scannerクラスのインスタンスを作成
 		//引数:標準入力System.in
 		Scanner scannerInt=new Scanner(System.in);
-		//整数の入力を促す
-		System.out.print("何個*を表示しますか:");
-		//インスタンスから入力された値を受け取る
-		int inputValue=scannerInt.nextInt();
-		//カウント用変数が入力値より大きくなったらループ抜け
+		
+		//入力値を代入する変数を宣言
+		int inputValue;
+		//後判定ループ文
+		do {
+			//整数の入力を促す
+			System.out.print("何個*を表示しますか:");
+			//インスタンスから入力された値を受け取る
+			inputValue=scannerInt.nextInt();
+		}
+		//正の数が入力されたらループ抜け
+		while(inputValue<=0);
+
+		
+		//カウント用変数が入力値を超えたらループ抜け
 		for(int countValue=0;countValue<inputValue;countValue++) {
 			//*を表示
 			System.out.print('*');
-			//入力値が1以上の場合
-			}if(inputValue>=1)
-				//改行を出力
-				System.out.println();
+		}
+		//入力値が1以上の場合
+		if(inputValue>=1) {
+			//改行を出力
+			System.out.println();
+		}
 	}
 }
