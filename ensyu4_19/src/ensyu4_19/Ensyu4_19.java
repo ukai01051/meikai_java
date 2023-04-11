e9bf56b8669e864da3e734ef68c6f4466960ac26
ukai01051 <a@abc.com>
2023-04-11 16:32:39 +0900

    演習4-19　修正しました。

--- ensyu4_19/src/ensyu4_19/Ensyu4_19.java
+++ ensyu4_19/src/ensyu4_19/Ensyu4_19.java
@@ -21,6 +21,8 @@ public class Ensyu4_19 {
 		//Scannerクラスのインスタンスを作成
 		//引数:標準入力System.in
 		Scanner scannerInteger=new Scanner(System.in);
+		
+		//入力値を代入する変数を宣言
 		int inputValue;
 		//後判定ループ文
 		do {
@@ -32,35 +34,27 @@ public class Ensyu4_19 {
 				inputValue=scannerInteger.nextInt();
 			//入力値が1～12の場合ループ抜け
 			}while(inputValue<1 || inputValue>12);
-		//入力値が負の数の場合ループ抜け
+		//入力値が正の数の場合ループ抜け
 		}while(inputValue<0);
 		
 		//入力値の場合分け
 		switch(inputValue) {
 		//3～5の場合
-		case 3,4,5:
-			//春を表示
-			System.out.println("季節は春です。");
-		//プログラム終了
-		break;
+		case 3,4,5: System.out.println("季節は春です。");
+					//プログラム終了
+					break;
 		//6～8の場合
-		case 6,7,8:
-			//夏を表示
-			System.out.println("季節は夏です。");
-		//プログラム終了
-		break;
+		case 6,7,8: System.out.println("季節は夏です。");
+					//プログラム終了
+					break;
 		//9～11の場合
-		case 9,10,11:
-			//秋を表示
-			System.out.println("季節は秋です。");
-		//プログラム終了
-		break;
+		case 9,10,11:System.out.println("季節は秋です。");
+					//プログラム終了
+					break;
 		//12～2の場合
-		case 12,1,2:
-			//冬を表示
-			System.out.println("季節は冬です。");
-		//プログラム終了
-		break;
+		case 12,1,2:System.out.println("季節は冬です。");
+					//プログラム終了
+					break;
 		}
 	}
 }
