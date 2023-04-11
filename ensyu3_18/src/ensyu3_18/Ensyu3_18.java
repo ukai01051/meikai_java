d437b111649ab5db0c4155615899d51e2c58d579
ukai01051 <a@abc.com>
2023-04-11 09:05:29 +0900

    演習3-18 修正しました。

--- ensyu3_18/src/ensyu3_18/Ensyu3_18.java
+++ ensyu3_18/src/ensyu3_18/Ensyu3_18.java
@@ -30,24 +30,24 @@ public class Ensyu3_18 {
 		switch(monthValue) {
 		//3,4,5の場合を表示
 		case 3,4,5 :System.out.println("季節は春です。");
-		//プログラム終了
-		break;
+					//プログラム終了
+					break;
 		//6,7,8の場合夏を表示
 		case 6,7,8 :System.out.println("季節は夏です。");
-		//プログラム終了
-		break;
+					//プログラム終了
+					break;
 		//9,10,11の場合秋を表示
 		case 9,10,11 :System.out.println("季節は秋です。");
-		//プログラム終了
-		break;
+					//プログラム終了
+					break;
 		//12,1,2の場合冬を表示
 		case 12,1,2 :System.out.println("季節は冬です。");
-		//プログラム終了
-		break;
+					//プログラム終了
+					break;
 		//不正な値入力
 		default : System.out.println("不正な値が入力されました。");
-		//プログラム終了
-		break;
+					//プログラム終了
+					break;
 		}
 
 	}
