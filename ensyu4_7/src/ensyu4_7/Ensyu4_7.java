3a716586bfbd33fa374a8deada82c24828350ae2
ukai01051 <a@abc.com>
2023-04-11 17:09:18 +0900

    演習4-7 修正しました。

--- ensyu4_7/src/ensyu4_7/Ensyu4_7.java
+++ ensyu4_7/src/ensyu4_7/Ensyu4_7.java
@@ -21,26 +21,35 @@ public class Ensyu4_7 {
 		//Scannerクラスのインスタンスを作成
 		//引数:標準入力System.in
 		Scanner scannerInt=new Scanner(System.in);
-		//整数の入力を促す
-		System.out.print("何個表示しますか:");
-		//インスタンスから入力された値を受け取る
-		int inputNumber=scannerInt.nextInt();
+		//入力値を代入する変数を宣言
+		int inputValue;
+		//後判定ループ文
+		do {
+			//整数の入力を促す
+			System.out.print("何個表示しますか:");
+			//インスタンスから入力された値を受け取る
+			inputValue=scannerInt.nextInt();
+		//正の数が入力されたらループ抜け
+		}while(inputValue<=0);
+
+		
 		//出力用の変数を宣言して初期化
-		int outputNumber=0;
+		int outputValue=0;
+		
 		//前判定ループ文
 		//出力用の変数が入力値と等しくなったらループ抜け
-		while(inputNumber>outputNumber) {
+		while(inputValue>outputValue) {
 			//偶数の場合
-			if(outputNumber%2==0) {
-				//+を表示
+			if(outputValue%2==0) {
+				//*を表示
 				System.out.print('*');
 			//奇数の場合
 			}else {
-				//＊を表示
+				//+を表示
 				System.out.print('+');
 			}
 			//出力用の変数をインクリメント
-			outputNumber++;
+			outputValue++;
 		}
 	}
 
