ce1063cf28a719bf9169b4af7eea8ac4bcf13ed0
ukai01051 <a@abc.com>
2023-04-11 16:59:22 +0900

    演習4-9 修正しました。

--- ensyu4_9/src/ensyu4_9/Ensyu4_9.java
+++ ensyu4_9/src/ensyu4_9/Ensyu4_9.java
@@ -4,14 +4,14 @@ package ensyu4_9;
 import java.util.Scanner;
 /*
  * クラス名:Ensyu_
- * 概要:1～入力された値までの積を表示
+ * 概要:1～入力値までの積を表示
  * 作成者:ukai
  * 作成日:2023/04/07
  */
 public class Ensyu4_9 {
 	/*
 	 * 関数名:main
-	 * 概要:1～入力された値までの積を表示
+	 * 概要:1～入力値までの積を表示
 	 * 引数:なし
 	 * 戻り値:なし
 	 * 作成者:ukai
@@ -21,10 +21,26 @@ public class Ensyu4_9 {
 		//Scannerクラスのインスタンスを作成
 		//引数:標準入力System.in
 		Scanner scannerInt=new Scanner(System.in);
-		//整数の入力を促す
-		System.out.print("整数値:");
-		//インスタンスから入力された値を受け取る
-		int inputValue=scannerInt.nextInt();
+		
+		//入力値を代入する変数を宣言
+		int inputValue;
+		//概要説明を表示
+		System.out.println("1からnまでの積を表示します。");
+		
+		//後判定ループ文
+		do {
+			//整数の入力を促す
+			System.out.print("nの値:");
+			//インスタンスから入力された値を受け取る
+			inputValue=scannerInt.nextInt();
+			//
+			if(inputValue>12) {
+				//
+				System.out.println("nが13以上だと正常に表示できません。");
+			}
+		//正の数が入力されたらループ抜け
+		}while(inputValue<=0);
+
 		//カウント用変数countValueを宣言して初期化
 		int countValue=1;
 		//合計用変数totalValueを宣言して初期化
