070f618ef7722dee5f44486d87f495b5a64da327
ukai01051 <a@abc.com>
2023-04-12 14:32:02 +0900

    演習4-22

---
+++ ensyu4_22/src/ensyu4_22/Ensyu4_22.java
@@ -0,0 +1,59 @@
+//ensyu4_22のパッケージ宣言
+package ensyu4_22;
+//標準入力のためのインポート文
+import java.util.Scanner;
+/*
+ * クラス名:Ensyu4_22
+ * 概要:＊で入力された数値の段のピラミッドを作成して表示
+ * 作成者:ukai
+ * 作成日:2023/04/11
+ */
+public class Ensyu4_22 {
+	/*
+	 * 関数名:main
+	 * 概要:＊で入力された数値の段のピラミッドを作成して表示
+	 * 引数:なし
+	 * 戻り値:なし
+	 * 作成者:ukai
+	 * 作成日:2023/04/11
+	 */
+	public static void main(String[] args) {
+		
+		//Scannerクラスのインスタンスを作成
+		//引数:標準入力System.in
+		Scanner scannerInteger=new Scanner(System.in);
+		//入力値を代入する変数を宣言
+		int inputValue;
+		
+		//概要説明を表示
+		System.out.println("n段のピラミッドを作成します。");
+		//後判定ループ文
+		do {
+			//整数の入力を促す
+			System.out.print("nの値:");
+			//インスタンスから入力された値を受け取る
+			inputValue=scannerInteger.nextInt();
+		//正の数が入力されたらループ抜け
+		}while(inputValue<=0);
+		
+		//改行回数と＊表示回数の変数を宣言
+		int heightValue,widthValue;
+		
+		//改行回数が入力値を超えたらループ抜け
+		for(heightValue=1;heightValue<=inputValue;heightValue++) {
+			
+			//*表示が入力値と改行回数の差を超えたらループ抜け
+			for(widthValue=1;widthValue<=inputValue-heightValue;widthValue++) {
+				//空白を表示
+				System.out.print(" ");
+			}
+			//＊表示が改行回数を超えたらループ抜け
+			for(widthValue=1;widthValue<=(heightValue-1)*2+1;widthValue++) {
+				//*表示
+				System.out.print("*");
+			}
+			//改行を表示
+			System.out.println();
+		}
+	}
+}
